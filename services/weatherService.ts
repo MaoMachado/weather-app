dc8e5d5a38aff3278cf8c52f26b464ef052f491c
Mao.Dev <114309283+MaoMachado@users.noreply.github.com>
2026-07-22 16:17:19 -0500

    fix: corrección sintaxis @tailwind, comentarios borrados

--- services/weatherService.ts
+++ services/weatherService.ts
@@ -21,22 +21,3 @@ export const getForecast = async (city: string) => {
   });
   return response.data;
 };
-
-// export async function getForecast(city: string) {
-//   try {
-//     const { data } = await axios.get(`${process.env.WEATHERAPI_KEY}`, {
-//       params: {
-//         q: city,
-//         days: 3,
-//         aqi: "yes",
-//         alert: "yes",
-//       },
-//     });
-
-//     return console.log(data);
-//   } catch (err) {
-//     console.error(`Error: ${err}`);
-//     alert("Ingresa una ciudad");
-//     return;
-//   }
-// }
